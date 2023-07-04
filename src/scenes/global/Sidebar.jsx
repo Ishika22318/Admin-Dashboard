fe24e45c12ad7e2498ac595d700a19ce22953069
Ishika22318 <102206596+Ishika22318@users.noreply.github.com>
2023-07-04 20:46:28 +0530

    commit after change

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -108,10 +108,10 @@ const Sidebar = () => {
                   fontWeight="bold"
                   sx={{ m: "10px 0 0 0" }}
                 >
-                  Ed Roh
+                  Ishika Agarwal
                 </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
-                  VP Fancy Admin
+                  Admin
                 </Typography>
               </Box>
             </Box>
